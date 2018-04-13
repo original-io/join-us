5d33a6fa7fb3693409dbf4cb91e19acca3b369ea
Aldiran Santos <contact.aldiransantos@gmail.com>
2018-04-13 02:39:43 -0300

    Entrega final
    Criação de arquivo palette.sass para colocar as cores usadas no site.
    Finalização da organização do style.sass
    Ajustes em componentes do site.
    Otimização das imagens e minificação do css.

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,16 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task('default', ['css', 'sass', 'img']);
+gulp.task('default', ['sass', 'img']);
 
 gulp.task('watch', function() {
   gulp.watch('./dev/styles/sass/*.sass', ['sass']);
 });
 
 gulp.task('sass', function () {
-  return gulp.src('dev/styles/sass/*.sass')
+  return gulp.src('dev/styles/sass/style.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
 });
@@ -22,12 +23,13 @@ gulp.task('image', function(){
     .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('css', function () {
-  gulp.src('./dist/css/*.css')
-    .pipe(uglifycss({
-      "maxLineLen": 80,
-      "uglyComments": true
+gulp.task('minify-css', function(){
+  return gulp.src(['./dist/css/*.css', './dev/styles/css/*.css'])
+    .pipe(cleanCSS({debug: true}, function(details){
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
     }))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('dist/css'));
 });
+
