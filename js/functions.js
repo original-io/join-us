4c0e00c8f14952789b26d47ab514a04faca18b11
renanlimabl <39681555+renanlimabl@users.noreply.github.com>
2018-06-17 19:23:25 -0300

    Terminando site, só falta organizar código.

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-    // Selecao de produtos - Slider Lateral / Foto grande.
+ // Selecao de produtos - Slider Lateral / Foto grande.
     
     let produtoSlider = document.getElementById('produtoSlider').getElementsByTagName('img');
     let produtoSliderGrande = document.getElementById('produtoSliderGrande');
@@ -13,7 +13,7 @@
         produtoSliderGrande.innerHTML = "<img src="+imgSrc+">";
     }
 
-    // Selecionar cores -  
+// Selecionar cores -  
     
    $(function(){
        $('.cores').click(function(){
@@ -22,7 +22,7 @@
        })
    })
 
-   // Selecionar tamanho
+// Selecionar tamanho
 
    $(function(){
     $('.tamanho').click(function(){
@@ -30,3 +30,57 @@
         $(this).addClass('tamanhoativo')
     })
 })
+
+// Menu mobile
+    $('.mobile--menuhamb').click(function(){
+        $('header nav.mobile ul').slideToggle();
+    })
+
+// modal
+
+let alturaTela = $(document).height()
+let larguraTela = $(document).width()
+
+$('#adicionarSacola').click(function(){
+    $('#mascaraModal').css({'width':larguraTela,'height':alturaTela})
+    $('#mascaraModal').fadeIn(1000)
+    $('#mascaraModal').fadeTo("slow",0.8)
+    $('.modal--sucesso').fadeIn(1000)
+})
+
+$('#modalFechar').click(function(){
+    $('#mascaraModal').hide()
+    $('.modal--sucesso').hide()
+})
+
+// SACOLA SUCESSO
+
+$('#sacolaSucesso').click(function(){
+    $('.sacola--sucesso').fadeIn(500);
+})
+$('.sacola--sucesso__fechar').click(function(){
+    $('.sacola--sucesso').hide();
+})
+
+// menu desktop
+
+$('#sapatos').click(function(){
+    $('.toggleSapatos').slideToggle(700);
+    $('.toggleBolsas').hide();
+    $('.toggleAcessorios').hide();
+})
+
+$('#bolsas').click(function(){
+    $('.toggleBolsas').slideToggle(700);
+    $('.toggleSapatos').hide();
+    $('.toggleAcessorios').hide();
+})
+
+$('#acessorios').click(function(){
+    $('.toggleAcessorios').slideToggle(700);
+    $('.toggleSapatos').hide();
+    $('.toggleBolsas').hide();
+})
+
+
+
