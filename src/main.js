db84c624e2b0f7815e3cb3ec9c8b70c93de7e31c
Marco Carvalho <marcolucio27@gmail.com>
2018-06-19 11:23:06 -0300

    import dependencies

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap"
+import "@/scss/custom.scss"
+import "@fortawesome/fontawesome"
+import "@fortawesome/fontawesome-free-solid/"
+import "@fortawesome/fontawesome-free-brands/"
+import "@fortawesome/fontawesome-free-regular/"
+
 Vue.config.productionTip = false
 
 new Vue({
