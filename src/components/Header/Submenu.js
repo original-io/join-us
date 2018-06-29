38bdc18d73e503a26b06bc4d3df2b04f39db01ef
Glauber Mota <glaubernm@gmail.com>
2018-06-29 05:53:58 -0300

    Fonte do preço e minor fix no submenu (alt da img)

--- src/components/Header/Submenu.js
+++ src/components/Header/Submenu.js
@@ -32,7 +32,7 @@ class Submenu extends Component {
                       </ul>
                     </div>
                     <div className="col-md-4">
-                      <img className="img-fluid submenu__img" alt="{item.title}" src={require("../../assets/img/header/submenu.png")} />
+                      <img className="img-fluid submenu__img" alt={item.title} src={require("../../assets/img/header/submenu.png")} />
                     </div>
                   </div>
                 </div>
