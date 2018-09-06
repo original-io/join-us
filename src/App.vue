c5a143b85a444960d36f7f6431e20f93310fbb8d
MoreiraVinicius <vinicius.ads.mec@gmail.com>
2018-09-05 23:29:58 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Header from "./components/Header";
+import Buefy from "buefy";
+import "buefy/lib/buefy.css";
+
+Vue.use(Buefy, {
+  defaultIconPack: "fa"
+});
+
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang =scss>
+// Import Bulma's core
+@import "./sass/spacing.scss";
+@import "~bulma/sass/utilities/_all";
+
+body {
+  background-color: $white;
+}
+// colors
+$primary: #de8f75;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099ff;
+$twitter-invert: findColorInvert($twitter);
+
+//fonts
+$family-serif: "Nunito", serif;
+
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+  "white": ($white, $black),
+  "black": ($black, $white),
+  "light": ($light, $light-invert),
+  "dark": ($dark, $dark-invert),
+  "primary": ($primary, $primary-invert),
+  "info": ($info, $info-invert),
+  "success": ($success, $success-invert),
+  "warning": ($warning, $warning-invert),
+  "danger": ($danger, $danger-invert),
+  "twitter": ($twitter, $twitter-invert)
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+@import url("https://fonts.googleapis.com/css?family=Nunito+Sans");
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container is-fluid is-marginless">
+        <div class="center" >
+             <img class="m-t-mdd m-b-mdd" src="../assets/logo.png" alt="Logo original.io" >
+        </div>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+
+            <div id="navbarBurger" class="navbar-burger m-t-sm m-b-sm" data-target="navMenu" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="nav-center navbar-menu">
+                <a v-for="item in items" :key="item" class="is-tab is-hidden-desktop navbar-item">{{item}}</a>    
+            </div>
+
+
+            <div id="navMenu"   class="nav-right navbar-menu">
+                <a v-for="item in items" :key="item" class="is-tab is-hidden-touch navbar-item m-r-sm m-l-sm">{{item}}</a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      items: ["Sapatos", "Bolsas", "Acessórios", "Off"]
+    };
+  }
+};
+</script>
+
+
+<style>
+.center {
+  text-align: center;
+  display: block;
+  width: 100%;
+}
+.navbar-item {
+  color: black;
+  text-decoration: none;
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 14px;
+  text-transform: uppercase;
+  color: #686868;
+}
+.navbar .navbar-brand > .navbar-item,
+.navbar .navbar-brand .navbar-link {
+  display: inline-block;
+}
+.navbar {
+  border-top: 1px solid #ccc;
+  border-bottom: 1px solid #ccc;
+}
+</style>
