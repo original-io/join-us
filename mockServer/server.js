18a7df3e409bf0692eb3554c9b75b57b4cb8b3ea
Leonardo Ferraz <ldlferraz@gmail.com>
2020-02-17 18:10:50 -0300

    Finalizando Produto (sem quem viu tbm ve, WIP)

--- mockServer/server.js
+++ mockServer/server.js
@@ -25,7 +25,7 @@ class API {
         );
         api.use(bodyParser.json());
         api.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "http://ec2-18-229-118-221.sa-east-1.compute.amazonaws.com");
+            res.header("Access-Control-Allow-Origin", "http://localhost:8080");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
