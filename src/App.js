f0a3dceb8bb110ce0cbadeed8b270bc2d6d406fc
tsuna2221 <tsuna2221@live.com>
2020-03-01 05:14:04 -0300

    footer init

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React from 'react';
 import Header from './components/Header'
 import Product from './components/Product'
 import Recommendations from './components/Recommendations'
+import Footer from './components/Footer'
 
 const App = () => (
     <div className='App'>
       <Header/>
       <Product/>
       <Recommendations/>
+      <Footer/>
     </div>
 )
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Assets
+import Facebook from '../../assets/facebook.png'
+import Instagram from '../../assets/instagram.png'
+import Logo from '../../assets/logo-white.png'
+import Pinterest from '../../assets/pinterest.png'
+import Ebit from '../../assets/selo-ebit.png'
+import VTEX from '../../assets/vtex.png'
+
+const Footer = () => (
+    <div className='Footer'>
+
+    </div>
+)
+
+export default Footer;
